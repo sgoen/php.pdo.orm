542c806d6408da729b944351ebdf42ba3965eefd
J.Smit <j.smit@sgoen.nl>
2010-11-03 03:41:42 +0100

    fixed indents

--- test/unittest/Test_Orm_Core.php
+++ test/unittest/Test_Orm_Core.php
@@ -8,88 +8,108 @@ require_once("bootstrap.php");
  */
 class Test_Orm_Core extends PHPUnit_Framework_TestCase
 {
-        protected $orm;
+	protected $orm;
 
-        public function setUp()
-        {
-                $sql = "DROP TABLE IF EXISTS `Item`;
-                 CREATE TABLE `Item` (
-                   `id` int(11) NOT NULL AUTO_INCREMENT,
-                   `name` varchar(55) NOT NULL,
-                   PRIMARY KEY (`id`)
-                 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=0";
+	public function setUp()
+	{
+		$sql = "DROP TABLE IF EXISTS `Item`;
+		    CREATE TABLE `Item` (
+		        `id` int(11) NOT NULL AUTO_INCREMENT,
+		        `name` varchar(55) NOT NULL,
+		        PRIMARY KEY (`id`)
+		    ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=0";
 
-                $this->orm = new Orm_Core_T();
-                $this->orm->query($sql);
+		$this->orm = new Orm_Core_T();
+		$this->orm->query($sql);
 
-                $itemOne = new Item();
-                $itemTwo = new Item();
+		$itemOne = new Item();
+		$itemTwo = new Item();
 
-                $itemOne->name = "Foo";
-                $itemTwo->name = "Bar";
+		$itemOne->name = "Foo";
+		$itemTwo->name = "Bar";
 
-                $this->orm->save($itemOne);
-                $this->orm->save($itemTwo);
-        }
+		$this->orm->save($itemOne);
+		$this->orm->save($itemTwo);
+	}
 
-        public function testGet()
-        {
-                $result = $this->orm->get("Item");
-                $this->assertEquals(2, count($result));
+	public function testGet()
+	{
+		$result = $this->orm->get("Item");
+		$this->assertEquals(2, count($result));
 
-                $result = $this->orm->get("Item", "WHERE name = :name", array("name" => "Foo"));
-                $this->assertEquals(1, count($result));
+		$result = $this->orm->get("Item", "WHERE name = :name", array("name" => "Foo"));
+		$this->assertEquals(1, count($result));
 
-                $result = $this->orm->get("Item", "WHERE name = :name", array("name" => "nonsense"));
-                $this->assertEquals(0, count($result));
-        }
+		$result = $this->orm->get("Item", "WHERE name = :name", array("name" => "nonsense"));
+		$this->assertEquals(0, count($result));
+	}
 
-		/**
-		 * @expectedException Exception
-		 */
-        public function testGetException()
-        {
-                $this->orm->get("Nonsense");
-        }
+	/**
+	 * @expectedException Exception
+	 */
+	public function testGetException()
+	{
+		$this->orm->get("Nonsense");
+	}
 
-        public function testSave()
-        {
-                // save a new item with name
-                $item = new Item();
-                $item->name = "TestSave1";
-                $this->orm->save($item);
-                $this->assertEquals(1, count($this->orm->get('Item', "WHERE name = :name", array("name" => "TestSave1"))));
-        }
+	public function testSave()
+	{
+		// save a new item with name
+		$item = new Item();
+		$item->name = "TestSave1";
+		$this->orm->save($item);
+		$this->assertEquals(1, count($this->orm->get('Item', "WHERE name = :name", array("name" => "TestSave1"))));
+	}
 
-		/**
-		 * @expectedException Exception
-		 */
-        public function testSaveException()
-        {
-                // save a new item with name and id (not existing in db)
-                $item = new Item();
-                $item->id = 111;
-                $item->name = "TestSaveException";
-                $this->orm->save($item);
-        }
+	/**
+	 * @expectedException Exception
+	 */
+	public function testSaveException()
+	{
+		// save a new item with name and id (not existing in db)
+		$item = new Item();
+		$item->id = 111;
+		$item->name = "TestSaveException";
+		$this->orm->save($item);
+	}
 
-        public function testRemove()
-        {
-                $result = $this->orm->get('Item');
+	public function testRemove()
+	{
+		$result = $this->orm->get('Item');
 
-                // remove an item and test the returned amount of items.
-                $item = $result[0];
-                $this->orm->remove($item);
-                $result = $this->orm->get('Item');
+		// remove an item and test the returned amount of items.
+		$item = $result[0];
+		$this->orm->remove($item);
+		$result = $this->orm->get('Item');
 
-                $this->assertEquals(1, count($result));
+		$this->assertEquals(1, count($result));
 
-                // remove another item and test the returned amount of items.
-                $item = $result[0];
-                $this->orm->remove($item);
-                $result = $this->orm->get('Item');
+		// remove another item and test the returned amount of items.
+		$item = $result[0];
+		$this->orm->remove($item);
+		$result = $this->orm->get('Item');
 
-                $this->assertEquals(0, count($result));
-        }
+		$this->assertEquals(0, count($result));
+	}
+        
+	public function testTransaction()
+	{
+		$itemOne = new Item();
+		$itemTwo = new Item();
+        	
+		$itemOne->name = "one";
+		$itemTwo->name = "two";
+        	
+		$this->orm->startTransaction();
+        	
+		$this->orm->save($itemOne);
+		$this->orm->save($itemTwo);
+        	
+		$this->assertEquals(2, count($this->orm->get('Item')));
+        	
+		$this->orm->commitTransaction();
+        	
+		$this->assertEquals(4, count($this->orm->get('Item')));
+	}
 }
 ?>
